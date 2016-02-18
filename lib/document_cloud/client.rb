bab7385d3ec0a2ad3a5e79262452845a6bdc52ce
Caleb <caleb.tutty@nzherald.co.nz>
2016-02-19 10:09:01 +1300

    Automatic editor changes to trailing spaces and indentation

--- lib/document_cloud/client.rb
+++ lib/document_cloud/client.rb
@@ -23,38 +23,38 @@ module DocumentCloud
     include DocumentCloud::API::CreateProject
     include DocumentCloud::API::UpdateProject
     include DocumentCloud::Configurable
-    
+
     def initialize(options={})
       DocumentCloud::Configurable.keys.each do |key|
         instance_variable_set(:"@#{key}", options[key] || DocumentCloud.instance_variable_get(:"@#{key}"))
       end
     end
-    
+
     # Perform HTTP GET request
     def get(path, params={})
       RestClient.get request_base+path, {params: params}
     end
-    
+
     # Perform HTTP POST request
     def post(path, params={})
       RestClient.post request_base+path, params
     end
-    
+
     # Perform HTTP PUT request
     def put(path, params={})
       RestClient.put request_base+path, params
     end
-    
+
     # Perform HTTP DELETE request
     def delete(path)
       RestClient.delete request_base+path
     end
-    
+
     private
-    
-      def request_base
-        "#{DocumentCloud::Default.http_mode}://#{@email}:#{@password}@#{DocumentCloud::Default.endpoint}"
-      end
-    
+
+    def request_base
+      "#{DocumentCloud::Default.http_mode}://#{@email}:#{@password}@#{DocumentCloud::Default.endpoint}"
+    end
+
   end
 end
