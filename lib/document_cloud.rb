ce1ad8aa2a24e22e49a36780c22fc24a2b57ce26
Caleb <caleb.tutty@nzherald.co.nz>
2016-02-19 13:11:42 +1300

    Require page

--- lib/document_cloud.rb
+++ lib/document_cloud.rb
@@ -5,6 +5,7 @@ require 'open-uri'
 
 require_relative 'document_cloud/document'
 require_relative 'document_cloud/project'
+require_relative 'document_cloud/page'
 require_relative 'document_cloud/search_results'
 require_relative 'document_cloud/default'
 require_relative 'document_cloud/configurable'
@@ -13,7 +14,7 @@ require_relative 'document_cloud/client'
 module DocumentCloud
   class << self
     include DocumentCloud::Configurable
-    
+
     # Delegate to a DocumentCloud::Client
     #
     # @return [DocumentCloud::Client]
@@ -21,25 +22,25 @@ module DocumentCloud
       @client = DocumentCloud::Client.new(credentials) unless defined?(@client)
       @client
     end
-    
+
     # Has a client been initialized on the DocumentCloud module?
     #
     # @return [Boolean]
     def client?
       !!@client
     end
-    
-    
+
+
     def respond_to?(method_name, include_private=false)
       client.respond_to?(method_name, include_private) || super
     end
-    
+
     private
 
-      def method_missing(method_name, *args, &block)
-        return super unless client.respond_to?(method_name)
-        client.send(method_name, *args, &block)
-      end
-    
+    def method_missing(method_name, *args, &block)
+      return super unless client.respond_to?(method_name)
+      client.send(method_name, *args, &block)
+    end
+
   end
 end
